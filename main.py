f28b5c737868bff3cfb34deb01496a5004c6ae24
Jip Wulffelé <jipwulffele@gmail.com>
2025-09-22 14:16:33 +0200

    add start position

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from core.video_handler import VideoHandler
 from core.pose_detector import PoseDetector
 from core.pushup_counter import PushUpCounter
 from core.visualizer import Visualizer
+from core.position_validator import PositionValidator
 
 
 def main():
@@ -13,9 +14,11 @@ def main():
     detector = PoseDetector()
     counter = PushUpCounter()
     visualizer = Visualizer()
+    validator = PositionValidator()
 
     # Initialize count and stage
     count, stage = 0, "other"
+    start_ready = False # Not correct starting position
 
     while video.is_open():
         frame = video.get_frame()
@@ -24,13 +27,26 @@ def main():
         
         results = detector.detect(frame) # media pipe pose detection
 
-        if results.pose_landmarks:
-            count, stage = counter.update(results.pose_landmarks.landmark, method="logic")
-            frame = detector.draw(frame, results)
+        if not results.pose_landmarks: # Not started yet and nobody in view
+            if not start_ready:
+               frame = visualizer.draw_position_warning(frame)
+
+        elif results.pose_landmarks: # Person detected
+
+            if not start_ready:
+                start_ready = validator.check_start_position(results.pose_landmarks.landmark)
+                if not start_ready:
+                    frame = visualizer.draw_position_warning(frame)
+                else:
+                    print("✅ Starting position confirmed. Begin counting!")
+            
+            else: # ready for counting!
+                count, stage = counter.update(results.pose_landmarks.landmark, method="logic")
+                frame = detector.draw(frame, results)
         
-        # Show count and stage
-        frame = visualizer.draw_count(frame, count)
-        frame = visualizer.draw_stage(frame, stage)           
+        if start_ready: # Show count and stage
+            frame = visualizer.draw_count(frame, count)
+            frame = visualizer.draw_stage(frame, stage)           
 
         video.show(frame)
 
