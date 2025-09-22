42e8e76e1c0eec5fbcdc888d75558a6f7ff160d3
Jip Wulffelé <jipwulffele@gmail.com>
2025-09-22 11:09:16 +0200

    video aith landmarks and counter

--- core/visualizer.py
+++ core/visualizer.py
@@ -1,5 +1,39 @@
-# modifies camera feed, count reps
+import cv2
 
 class Visualizer():
     def __init__(self):
         pass
+
+    def draw_count(self, frame, count):
+
+        w, h, _ = frame.shape
+
+        # Rectangle dimensions (top-left corner, 1/5 width and 1/10 height)
+        x1, y1 = 0, 0
+        x2, y2 = w // 3, h // 8
+
+        text = f"Push ups: {count}"
+
+        frame = cv2.rectangle(frame,(x1, y1), (x2, y2),(0,165,255), -1)
+        
+        # Adjust fontSize so it fits
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 2
+        thickness = 2
+
+        (text_w, text_h), baseline = cv2.getTextSize(text, font, font_scale, thickness)
+
+        # Shrink font scale if text is too wide
+        while text_w > (x2 - x1 - 10):  # padding of 10 px
+            font_scale -= 0.1
+            (text_w, text_h), baseline = cv2.getTextSize(text, font, font_scale, thickness)
+
+        # Center text inside rectangle
+        text_x = x1 + (x2 - x1 - text_w) // 2
+        text_y = y1 + (y2 - y1 + text_h) // 2
+
+        # Put text (black)
+        frame = cv2.putText(frame, text, (text_x, text_y), font,
+                            font_scale, (0, 0, 0), thickness, cv2.LINE_AA)
+
+        return frame
---
+++ main.py
@@ -0,0 +1,40 @@
+import cv2
+
+from core.video_handler import VideoHandler
+from core.pose_detector import PoseDetector
+from core.pushup_counter import PushUpCounter
+from core.visualizer import Visualizer
+
+
+def main():
+
+    # Initiate video, detector, counter and visualizer
+    video = VideoHandler(source=0) # 0 default webcam
+    detector = PoseDetector()
+    counter = PushUpCounter()
+    visualizer = Visualizer()
+
+    while video.is_open():
+        frame = video.get_frame()
+        if frame is None:
+            break
+
+        
+        results = detector.detect(frame) # media pipe pose detection
+
+        if results.pose_landmarks:
+            #count = counter.update(results.pose_landmarks.landmark)
+            count = 100
+            frame = detector.draw(frame, results)
+            frame = visualizer.draw_count(frame, count)        
+
+        video.show(frame)
+
+        if video.should_quit('q'):
+            break
+
+    video.release()
+
+
+if __name__ == "__main__":
+    main()
